f193878c9ffbe46616abfe161ff029327f13ebec
Will Richman <williamrichman@gmail.com>
2014-10-06 11:43:57 -0700

    Use a single post subscription to ensure that we can always have the right post

--- server/publications.js
+++ server/publications.js
@@ -2,6 +2,9 @@ Meteor.publish('posts', function(options) {
   return Posts.find({}, options);
 });
 
+Meteor.publish('singlePost', function(id) {
+  return id && Posts.find(id);
+});
 
 Meteor.publish('comments', function(postId) {
   return Comments.find({postId: postId});
